be29711c42516d216fb66a053ff062f1db250b62
grep-michael <grep-michael@gmail.com>
2026-05-10 19:03:55 -0400

    test

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-EXEC_NAME="govdeals-bot"
+EXEC_NAME="webbot"
 USER=carpet
 HOST=10.0.0.185
 
 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o $EXEC_NAME .
 scp $EXEC_NAME $USER@$HOST:~/$EXEC_NAME
+
+rm $EXEC_NAME
